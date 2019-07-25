b1081aeb407548f2fcb6ccc68b7878e89c5be1f3
dechenpema <53275324+dechenpema@users.noreply.github.com>
2019-07-25 10:15:55 -0400

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,14 @@
+
+<script>
+    function changeImage() {
+
+        if (document.getElementById("imgClickAndChange").src == "IMG_2417.jpg")
+        {
+            document.getElementById("imgClickAndChange").src = "IMG_2123.jpg";
+        }
+        else
+        {
+            document.getElementById("imgClickAndChange").src = "IMG_2417.jpg";
+        }
+    }
+</script>
